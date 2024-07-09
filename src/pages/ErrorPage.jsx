b2b4beeb8c17451a4851c7cf1246604b8a10c7d0
Omojuwa Babatunde <omojuwababatunde1@gmail.com>
2024-07-09 03:59:19 +0100

    add an error page

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 const ErrorPage = () => {
   return (
-    <div className='max-w-[1440px] md:mx-auto bg-[#9C0001] font-bold text-white flex flex-col gap-6 justify-center items-center h-screen'>
+    <div className='max-w-[1440px] md:mx-auto md:px-28 md:py-8 px-4 py-8 bg-[#9C0001] font-bold text-white flex flex-col gap-6 justify-center items-center h-screen'>
       <h1 className='text-[200px] font-extrabold drop-shadow-2xl'>404</h1>
       <p>WE ARE SORRY BUT THE PAGE YOU REQUESTED WAS NOT FOUND</p>
       <button className='rounded-full bg-white text-[#9C0001] py-2 px-4'><Link to='/'>Back Home</Link></button>
